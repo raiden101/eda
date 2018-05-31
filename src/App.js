09610ae63de3baf3d54300393c8c9f708784c4d8
acnologia <shrikanthbudya@gmail.com>
2018-05-31 13:27:09 +0530

    Added create-react-app files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        E-pariksha
+      </div>
+    );
+  }
+}
+
+export default App;
