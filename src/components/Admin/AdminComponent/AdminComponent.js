9754d2864161571fb19f6238e1bfc64efca2d7a4
acnologia <shrikanthbudya@gmail.com>
2018-06-01 22:03:20 +0530

    Added selection input to select the functionality.

--- src/components/Admin/AdminComponent/AdminComponent.js
+++ src/components/Admin/AdminComponent/AdminComponent.js
@@ -4,111 +4,45 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
-import RenderTable from '../../RenderTable/RenderTable';
-import Modal from '../../Modal/Modal';
+import SelectedSlots from '../SelectedSlots/SelectedSlots';
 
 class AdminComponent extends Component{
-    state = {
-        duration: 0,
-        table: {
-            morning: [],
-            afternoon:[]
-        }
-    }
     constructor(props) {
         super(props);
         this.token = JSON.parse(localStorage.getItem("auth")).token;
-        this.tableHeads = [
-            "Date",
-            "Total slots",
-            "Remaining Slots",
-            "Selected Members"
-        ];
     }
-    componentDidMount() {
-        axios.post('http://localhost:5000/api/admin/', {
-            token:this.token
-        }).then((data) => {
-            data = data.data;
-            if (data.error !== null) {
-                this.setState({
-                    ...this.state,
-                    redirect: true
-                });
-            } else {
-                this.setState({
-                    ...this.state,
-                    table: {
-                        morning: data.data[0],
-                        afternoon:data.data[1]
-                    }
-                })
-            }
-        })
+    state = {
+        type:0
     }
-    changeDuration = ({target:{name,value}}) => {
+    changeDuration = ({ target: { name, value } }) => {
         this.setState({
             ...this.state,
-            [name]:value
+            [name]: value
         })
     }
-    translateSlotData(obj) {
-        let date = new Date(obj.date)
-        let dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()
-        return [
-            dateString,
-            obj.total_slot,
-            obj.remaining_slot,
-            <Modal
-                trigger={
-                    <Fragment>
-                        see details (
-                        <span className="fake-link">
-                            {obj.selected_members.length}
-                        </span>)
-                    </Fragment>
-                }
-                title={"Selected members"}
-                content={obj.selected_members.map((element, index) => {
-                    return <span className="wid-10 cell" key={"selected"+index}>{element}</span>
-                })}
-            />]
-    }
+
     render() {
-        let redirect = this.state.redirect ? <Redirect to="/" /> : null;
+        let type = this.state.type;
         return (
             <Fragment>
-                {redirect}
                 <div className="admin-component">
                     <div className="controls">
-                        <FormControl className="select-duration">
-                            <InputLabel>Duration</InputLabel>
+                        <FormControl className="data-functions">
+                            <InputLabel>Operation</InputLabel>
                             <Select
-                                value={this.state.duration}
+                                value={this.state.type}
                                 onChange={this.changeDuration}
                                 inputProps={{
-                                    name: 'duration'
+                                    name: 'type'
                                 }}>
-                                <MenuItem value={0}>Morning</MenuItem>
-                                <MenuItem value={1}>Afternoon</MenuItem>
+                                <MenuItem value={0}>Display Slot Selection</MenuItem>
+                                <MenuItem value={1}>Delete Users</MenuItem>
                             </Select>
                         </FormControl>
                     </div>
-                    {this.state.table.morning.length?this.state.duration ? <RenderTable
-                        data={this.state.table.afternoon}
-                        translate={this.translateSlotData}
-                        heads={this.tableHeads}
-                        /> : <RenderTable
-                            data={this.state.table.morning}
-                            translate={this.translateSlotData}
-                            heads={this.tableHeads}
-                        /> :
-                        <div className="loading">
-                            Loading..
-                        </div>
-                    }
+                    <div className="details">
+                        {!type && <SelectedSlots />}
+                    </div>
                 </div>
             </Fragment>
         );
---
+++ src/components/Admin/SelectedSlots/SelectedSlots.js
@@ -0,0 +1,113 @@
+import React, { Component, Fragment } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import axios from 'axios';
+import RenderTable from '../../RenderTable/RenderTable';
+import Modal from '../../Modal/Modal';
+import './SelectedSlots.css';
+class SelectedSlots extends Component {
+    state = {
+        duration: 0,
+        table: {
+            morning: [],
+            afternoon: []
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.token = JSON.parse(localStorage.getItem("auth")).token;
+        this.tableHeads = [
+            "Date",
+            "Total slots",
+            "Remaining Slots",
+            "Selected Members"
+        ];
+    }
+
+    changeDuration = ({ target: { name, value } }) => {
+        this.setState({
+            ...this.state,
+            [name]: value
+        })
+    }
+    translateSlotData(obj) {
+        let date = new Date(obj.date)
+        let dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()
+        return [
+            dateString,
+            obj.total_slot,
+            obj.remaining_slot,
+            <Modal
+                trigger={
+                    <Fragment>
+                        see details (
+                        <span className="fake-link">
+                            {obj.selected_members.length}
+                        </span>)
+                    </Fragment>
+                }
+                title={"Selected members"}
+                content={obj.selected_members.map((element, index) => {
+                    return <span className="wid-10 cell" key={"selected" + index}>{element}</span>
+                })}
+            />
+        ]
+    }
+    componentDidMount() {
+        axios.post('http://localhost:5000/api/admin/', {
+            token: this.token
+        }).then((data) => {
+            data = data.data;
+            if (data.error !== null) {
+                this.setState({
+                    ...this.state
+                });
+            } else {
+                this.setState({
+                    ...this.state,
+                    table: {
+                        morning: data.data[0],
+                        afternoon: data.data[1]
+                    }
+                })
+            }
+        })
+    }
+    render() {
+        return (
+            <Fragment>
+                <div className="controls">
+                    <FormControl className="select-duration">
+                        <InputLabel>Duration</InputLabel>
+                        <Select
+                            value={this.state.duration}
+                            onChange={this.changeDuration}
+                            inputProps={{
+                                name: 'duration'
+                            }}>
+                            <MenuItem value={0}>Morning</MenuItem>
+                            <MenuItem value={1}>Afternoon</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+                {this.state.table.morning.length ? this.state.duration ? <RenderTable
+                    data={this.state.table.afternoon}
+                    translate={this.translateSlotData}
+                    heads={this.tableHeads}
+                /> : <RenderTable
+                        data={this.state.table.morning}
+                        translate={this.translateSlotData}
+                        heads={this.tableHeads}
+                    /> :
+                <div className="loading">
+                    Loading..
+                </div>
+            }
+            </Fragment>
+        );
+    }
+}
+export default SelectedSlots;
