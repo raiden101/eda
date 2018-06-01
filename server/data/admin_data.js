477a2211113bc4f27c453d1934665fae8f32a882
raiden101 <prabhusachin44@gmail.com>
2018-06-01 18:30:49 +0530

    added token verification feature

--- server/data/admin_data.js
+++ server/data/admin_data.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const { aft_exam, morn_exam, faculty } = require('../schemas/collections')
-
+const jwt = require('jsonwebtoken');
+const { key } = require('../../credentials/credentials');
 
 // response will be an array of 2 arrays ie [[],[]]
 // 1st array will have all info about morning exams.
 // 2nd array """""""""""""""""""""""" aft exams.
-router.get('/', (req, res) => {
+const check_token = (req, res, next) => {
+  jwt.verify(req.body.token, key, (err, data) => {
+    if(err)
+      res.json({data: null, error: "error while fetching data!!"});
+    else
+      if(data.admin == 1)
+        next();
+  })
+}
+router.post('/', check_token,  (req, res) => {
   let p1 = morn_exam.find({}),
       p2 = aft_exam.find({});
   Promise.all([p1, p2])
@@ -25,10 +35,10 @@ router.post('/new_faculty', (req, res) => {
   .catch((err) => res.json({data: null, error: "error while adding to db!!"}))
 });
 
-router.get('/get_all_faculties', (req, res) => {
+router.post('/get_all_faculties', check_token, (req, res) => {
   faculty.find({})
   .then(data => res.json({data: data, error: null}))
-  .catch(err => res.json({data: null, error: "error while getting data!!"}));
+  .catch(err => res.json({data: null, error: "error while fetching data!!"}));
 });
 
 router.delete('/delete_faculty/:fac_id', (req, res) => {
