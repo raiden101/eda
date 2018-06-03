7567653e7ca9199450a24832b1edef1357dbb2fb
raiden101 <prabhusachin44@gmail.com>
2018-06-03 20:49:52 +0530

    changes in faculty init fetch, not osum tho

--- server/data/faculty_data.js
+++ server/data/faculty_data.js
@@ -16,10 +16,10 @@ const check_token = (req, res, next) => {
   }
 };
 
-// { token: '........'}
-router.post('/:fac_id', check_token, (req, res) => {
+// { token: '........', fac_id: ".."}
+router.post('/', check_token, (req, res) => {
   let resp = {};
-  faculty.findOne({fac_id: req.params.fac_id}, 'fac_des')
+  faculty.findOne({fac_id: req.body.fac_id}, 'fac_des')
   .then(data => {
     if(data != null) 
       return slot_limitation.findOne({fac_des: data.fac_des}, 'morn_max aft_max')
@@ -29,12 +29,12 @@ router.post('/:fac_id', check_token, (req, res) => {
   .then(data => {
     resp['morn_max'] = data.morn_max;
     resp['aft_max'] = data.aft_max;      
-    return morn_exam.find({selected_members: {$eq: req.params.fac_id}}, '-_id date').sort('date')
+    return morn_exam.find({selected_members: {$eq: req.body.fac_id}}, '-_id date').sort('date')
   })
   .then(
     data => {
       resp['morn_selection'] = data;
-      return aft_exam.find({selected_members: {$eq: req.params.fac_id}}, '-_id date').sort('date'); 
+      return aft_exam.find({selected_members: {$eq: req.body.fac_id}}, '-_id date').sort('date'); 
     }
   )
   .then(
