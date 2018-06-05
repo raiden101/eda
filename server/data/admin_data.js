d30c5755cecdc87931b9b6609e2ce154215b9a55
raiden101 <prabhusachin44@gmail.com>
2018-06-05 19:48:07 +0530

    added end point for getting data about a particular date

--- server/data/admin_data.js
+++ server/data/admin_data.js
@@ -221,4 +221,34 @@ router.post('/get_exam_timings', check_token, (req, res) => {
   .catch(err => res.json({error: "error while fetching", data: null}));
 })
 
+
+// { token: '...', date: '....', session: '...'}
+router.post('/slot_info', check_token, (req, res) => {
+  let _collection = req.body.session === 'morning' ? morn_exam : aft_exam;
+  _collection.aggregate([
+    { $match: { date: new Date(req.body.date) } },
+    { $project: { "selected_members": 1 } },
+    { $unwind: { path: "$selected_members" } },
+    { $lookup: {
+        from: 'faculties',
+        localField: "selected_members",
+        foreignField: "fac_id",
+        as: "fac_info"
+      } 
+    },
+    {
+      $project: {
+        "fac_info.fac_name": 1,
+        "fac_info.fac_des": 1, 
+        "fac_info.fac_id": 1,
+        "fac_info.branch": 1
+      }
+    }
+  ])
+  .then(data => res.json({data: data, error: null}))
+  .catch(err => res.json({error: "error while fetching data", data: null}));
+});
+
+
+
 module.exports = router;
