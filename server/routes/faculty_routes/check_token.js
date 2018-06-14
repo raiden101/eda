cfdb96c24c36d93152cac864d77b34d67f3b28c3
acnologia <shrikanthbudya@gmail.com>
2018-06-14 13:53:47 +0530

    made changes in check token for faculty.

--- server/routes/faculty_routes/check_token.js
+++ server/routes/faculty_routes/check_token.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
 			next();
 		} else res.json({ data: null, error: "auth error" });
 	} catch (err) {
-		res.json({ data: null, error: "error while fetching data!!" });
+		res.json({ data: null, error: "auth error" });
 	}
 };
 
