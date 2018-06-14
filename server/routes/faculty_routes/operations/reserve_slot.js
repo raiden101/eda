ebaae15e490805de1fb4f4885d939be368f3d592
raiden101 <prabhusachin44@gmail.com>
2018-06-14 13:48:22 +0530

    minor changes

--- server/routes/faculty_routes/operations/reserve_slot.js
+++ server/routes/faculty_routes/operations/reserve_slot.js
@@ -8,17 +8,25 @@ module.exports = (req, res) => {
 	let [_collection, field] = req.body.selected.session === 'morning' ? 
 	[morn_exam, 'morn_selected_slots'] : [aft_exam, 'aft_selected_slots'];
 	_collection.updateOne(
-		{ date: new Date(slot.date) }, 
+		{ 
+			$and: [ 
+				{ date: new Date(slot.date) }, 
+				{ remaining_slot: { $gt: 0 } }
+			] 
+		}, 
 		{ 
 			$push: { selected_members: req.fac_id }, 
 			$inc: { remaining_slot: -1 }
 		}
 	)
 	.then(data => {
-		return faculty.updateOne(
-			{ fac_id: req.fac_id }, 
-			{ $inc: { field: 1 } }
-		)
+		if(data.n === 0)
+			throw "error";
+		else
+			return faculty.updateOne(
+				{ fac_id: req.fac_id }, 
+				{ $inc: { field: 1 } }
+			)
 	})
 	.then(data => res.json({ data: "reservation successful", error: null}))
 	.catch(err => res.json({ error: "error while reserving slots", data: null}))
