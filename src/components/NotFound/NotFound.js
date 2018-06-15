13fb2f2093a08c068c53e94fe6ebf23cc9b7f835
acnologia <shrikanthbudya@gmail.com>
2018-06-15 20:13:35 +0530

    made changes to 404 page.

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,11 @@
 import React from 'react';
-
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 export default () => {
-    return ("404 Not Found");
+	return <div className="not-found table">
+			<div className="table-cell">
+				<div className="not-found-header">404</div>
+				Navigate your way back to <Link to="/">Home</Link>
+			</div>
+		</div>;
 }
