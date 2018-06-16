e7cf3bb1cf5073ee831616a17a956c322e31fdc3
acnologia <shrikanthbudya@gmail.com>
2018-06-16 11:22:18 +0530

    cleaner style in 404.

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './NotFound.css';
 import { Link } from 'react-router-dom';
 export default () => {
-	return <div className="not-found table">
-			<div className="table-cell">
-				<div className="not-found-header">404</div>
-				Navigate your way back to <Link to="/">Home</Link>
-			</div>
+	return <div className="not-found">
+			<div className="not-found-header">404 === Lost</div>
+			Navigate your way back to <Link to="/">Home</Link>
 		</div>;
 }
