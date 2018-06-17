b126717c82c3ad35fdf6cc51c8406b6846229eeb
raiden101 <prabhusachin44@gmail.com>
2018-06-17 13:01:52 +0530

    updated get_all_faculties endpoint

--- server/routes/admin_routes/operations/faculty_db_ops/get_all_faculties.js
+++ server/routes/admin_routes/operations/faculty_db_ops/get_all_faculties.js
@@ -2,7 +2,42 @@ const { faculty } = require('../../../../schemas/collections');
 
 // { token: '.......' }
 module.exports = (req, res) => {
-	faculty.find({}, req.body.fields.join(" "))
+	let proj = {}, fields = req.body.fields;
+	for(let i=0;i<fields.length;++i)
+		proj[fields[i]] = 1
+	
+	let query = req.body.pending ? [
+		{ 
+			$lookup: {
+				from: 'slot_limitations',
+				localField: 'fac_des',
+				foreignField: 'fac_des',
+				as: 'lims'
+			}
+		},
+		{ $unwind: '$lims' },
+		{
+			$project: {
+				'pending': {
+					$lt: [
+						{ $sum: ['$morn_selected_slots', '$aft_selected_slots'] },
+						'$lims.maximum'
+					]
+				},
+				...proj
+			}
+		}
+	] : 
+	[
+		{
+			$project: {
+				...proj
+			}
+		}
+	]
+
+	faculty.aggregate(query)
 	.then(data => res.json({data: data, error: null}))
-	.catch(err => res.json({data: null, error: "error while fetching data!!"}));
+	.catch(err => res.json({data: null, error: err}));
+	
 };
