b27e017f725ecd2fbca7632ebbecb33b6c51e5c9
raiden101 <prabhusachin44@gmail.com>
2018-06-18 16:41:50 +0530

    added rand pwd generation feature

--- server/routes/admin_routes/operations/faculty_db_ops/add_faculty.js
+++ server/routes/admin_routes/operations/faculty_db_ops/add_faculty.js
@@ -1,4 +1,22 @@
 const { faculty } = require('../../../../schemas/collections')
+const { random, floor } = Math;
+
+const pwd_options = [
+	'abcdefghijklmnopqrstuvwxyz',
+	'0123456789',
+	'@#',
+	'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+];
+const rand_range_int = (min, max) => floor(random()*(max-min+1)) + min;
+
+const get_rand_from = iter => iter[rand_range_int(0, iter.length-1)];
+
+const generate_password = () => {
+	let len = rand_range_int(4, 9), pwd = '';
+	for(let i=0;i<len;++i)
+		pwd += get_rand_from(get_rand_from(pwd_options));
+	return pwd.trim();
+}
 
 // { token: '.....', faculty_data: {.......} }
 
@@ -6,7 +24,10 @@ module.exports = (req, res) => {
 	faculty.findOne({fac_id: req.body.faculty_data.fac_id})
 	.then(data => {
 		if(data === null)
-			return new faculty(req.body.faculty_data).save();
+			return new faculty({
+				...req.body.faculty_data
+				// password: generate_password()
+			}).save();
 		return Promise.resolve(-1);
 	})
 	.then((data) => {
