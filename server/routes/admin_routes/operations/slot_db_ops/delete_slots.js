5e52136d63b0cdec71d0fdd3c878b7a580c52869
raiden101 <prabhusachin44@gmail.com>
2018-06-21 10:33:36 +0530

    updates in delete_slots

--- server/routes/admin_routes/operations/slot_db_ops/delete_slots.js
+++ server/routes/admin_routes/operations/slot_db_ops/delete_slots.js
@@ -1,4 +1,4 @@
-const { morn_exam, aft_exam } = require("../../../../schemas/collections");
+const { morn_exam, aft_exam, faculty } = require("../../../../schemas/collections");
 
 
 // { token: '.....', 
@@ -6,8 +6,25 @@ const { morn_exam, aft_exam } = require("../../../../schemas/collections");
 // session: "morning/afternoon"}
 
 module.exports = (req, res) => {
-	let _collection = req.body.session === "morning" ? morn_exam : aft_exam;
-	_collection.deleteMany({_id: {$in: req.body.slots_to_delete}})
+
+	let [_collection, prop] = req.body.session === "morning" ? 
+	[morn_exam, 'morn_selected_slots'] : [aft_exam, 'aft_selected_slots'];
+	
+	Promise.all(req.body.slots_to_delete.map(slot_id => {
+		return _collection.findById(slot_id, 'selected_members');
+	}))
+	.then(data => {
+		let facs = [];
+		for(let i=0;i<data.length;++i)
+			facs = facs.concat(data[i].selected_members);
+
+		return Promise.all(facs.map(fac_id => faculty.findOneAndUpdate(
+			{ fac_id: fac_id },
+			{ $inc: { [prop]: -1 } }
+		)))
+	})
+	.then(_ => _collection.deleteMany({_id: {$in: req.body.slots_to_delete}}))
 	.then(data => res.json({data: "slot deletion successfull", error: null}))
 	.catch(err => res.json({error: "error while deleting the slots", data: null}))
+
 };
