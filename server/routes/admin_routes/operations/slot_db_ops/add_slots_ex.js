b61e123ba15e17a83776b9c6892d5505cb947144
raiden101 <prabhusachin44@gmail.com>
2018-06-22 17:41:35 +0530

    minor bug fix

--- server/routes/admin_routes/operations/slot_db_ops/add_slots_ex.js
+++ server/routes/admin_routes/operations/slot_db_ops/add_slots_ex.js
@@ -18,22 +18,15 @@ module.exports = (req, res) => {
   // allowed date formats for excel
   // (YYYY/MM/DD)
 
-  const get_hours_and_mins = (date) => {
-    return {
-      'year': date.getFullYear(),
-      'date': date.getDate(),
-      'month': date.getMonth()
-    }
-  }
-  console.log(slots_data);
+
   Promise.all(slots_data.map(slot => {
     let _collection = slot[0] === 'morning' ? 
     morn_exam : aft_exam;  
-    console.log(get_hours_and_mins(new Date(slot[1])))
-    // return new _collection({
-    //   date: slot[1],
-    //   total_slot: slot[2]
-    // }).save();
+    
+    return new _collection({
+      date: new Date((date - (25567 + 1))*86400*1000),
+      total_slot: slot[2]
+    }).save();
   }))
   .then(data => res.json({ data: "data succesfully uploaded to db", error: null }))
   .catch(err => res.json({ error: "error while uploading to db", data: null }));
