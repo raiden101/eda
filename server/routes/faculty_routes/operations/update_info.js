021550b5f589898762cd63ee4d5971bed706c958
raiden101 <prabhusachin44@gmail.com>
2018-06-22 13:37:11 +0530

    minor change

--- server/routes/faculty_routes/operations/update_info.js
+++ server/routes/faculty_routes/operations/update_info.js
@@ -1,17 +1,18 @@
 const { faculty } = require('../../../schemas/collections');
 
 module.exports = (req, res) => {
+  let set_fields = {
+    contact_no: req.body.fac_data.contact_no,
+    fac_name: req.body.fac_data.fac_name,
+    branch: req.body.fac_data.branch,
+    email: req.body.fac_data.email
+  }
+  if(req.body.fac_data.password !== "" || req.body.fac_data.password !== null)
+    set_fields['password'] = req.body.fac_data.password;
+
 	faculty.updateOne(
     { fac_id: req.fac_id },
-    {
-      $set: {
-        contact_no: req.body.fac_data.contact_no,
-        password: req.body.fac_data.password,
-        fac_name: req.body.fac_data.fac_name,
-        branch: req.body.fac_data.branch,
-        email: req.body.fac_data.email
-      }
-    }
+    { $set: set_fields }
   )
   .then(data => res.json({ data: "updataion successful", error: null }))
   .catch(err => res.json({ error: "errow while updating", data: null }));
