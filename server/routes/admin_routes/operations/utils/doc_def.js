58c43e7dfd21ebabfadf77c7f291e9b732063d73
raiden101 <prabhusachin44@gmail.com>
2018-07-06 08:47:43 +0530

    update in pdf struct

--- server/routes/admin_routes/operations/utils/doc_def.js
+++ server/routes/admin_routes/operations/utils/doc_def.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const fl_logo = fs.readFileSync(path.join(__dirname, 'fl_logo.png'), 'base64');
+const nitte_logo = fs.readFileSync(path.join(__dirname, '/nitte_logo.png'), 'base64');
+
 module.exports = {
   content: [
+    {
+      image: nitte_logo,
+      width: 30, height: 30,
+      alignment: 'center'
+    },
     {
       text: 'Exam duty chart', 
       fontSize: 20, bold: true, alignment: 'center', lineHeight: 2
@@ -13,6 +23,15 @@ module.exports = {
           { text: 'Date(dd/mm/yyyy)', style: 'tableHeader' }]
         ]
       }
+    },
+    {
+      image: fl_logo,
+      width: 30, height: 30,
+      alignment: 'center'
+    },
+    {
+      text: "Powered by Finite Loop.", 
+      fontSize: 14, bold: false, alignment: 'center', lineHeight: 2
     }
   ],
   styles: {
