3bc4c0973a72b4ef5918485add7502a7540440e1
raiden101 <prabhusachin44@gmail.com>
2018-07-06 10:52:36 +0530

    minor bug fix

--- server/routes/admin_routes/operations/utils/doc_def.js
+++ server/routes/admin_routes/operations/utils/doc_def.js
@@ -1,33 +1,43 @@
 const fs = require('fs');
 const path = require('path');
-const fl_logo = fs.readFileSync(path.join(__dirname, 'fl_logo.png'), 'base64');
-const nitte_logo = fs.readFileSync(path.join(__dirname, '/nitte_logo.png'), 'base64');
+const nitte_logo = "data:image/png;base64," + fs.readFileSync(
+  path.join(__dirname, 'nitte_logo.png'), 'base64'
+);
+const fl_logo = "data:image/png;base64," + fs.readFileSync(
+  path.join(__dirname, 'fl_logo.png'), 'base64'
+);
 
 module.exports = {
+  header: {
+    image: nitte_logo,
+    width: 90,
+    alignment: 'center',
+    margin: [0, 10, 0, 15]
+  },
   content: [
     {
-      image: nitte_logo,
-      width: 30, height: 30,
-      alignment: 'center'
-    },
-    {
       text: 'Exam duty chart', 
-      fontSize: 20, bold: true, alignment: 'center', lineHeight: 2
+      margin: [0, 18, 0, 0],
+      fontSize: 20, 
+      alignment: 'center', lineHeight: 2
     },
     {
       table: {
         headerRows: 1,
         widths: ['*', '*'],
         body: [
-          [{ text: 'Session', style: "tableHeader" },
-          { text: 'Date(dd/mm/yyyy)', style: 'tableHeader' }]
+          [
+            { text: 'Session', style: "tableHeader" },
+            { text: 'Date(dd/mm/yyyy)', style: 'tableHeader' }
+          ]
         ]
       }
     },
     {
       image: fl_logo,
-      width: 30, height: 30,
-      alignment: 'center'
+      width: 60,
+      alignment: 'center',
+      margin: [0, 60, 0, 10]
     },
     {
       text: "Powered by Finite Loop.", 
@@ -44,7 +54,7 @@ module.exports = {
     row: {
       alignment: 'center',
       fontSize: 13,
-    }
+    },
   },
   pageMargins: [70, 60, 60, 70]
 }
